065b00dbfa6e84504075be888fc263c3826d917e
Jakob Vogelsang <jakob.vogelsang@omicronenergy.com>
2021-09-21 10:58:55 +0200

    refactor(action-component): move dynamic styles to render()

--- src/action-component.ts
+++ src/action-component.ts
@@ -2,47 +2,17 @@ import {
   css,
   customElement,
   html,
+  internalProperty,
   LitElement,
   property,
   TemplateResult,
 } from 'lit-element';
 
-import {
-  circuitBreakerIcon,
-  currentTransformerIcon,
-  disconnectorIcon,
-  earthSwitchIcon,
-  generalConductingEquipmentIcon,
-  voltageTransformerIcon,
-} from './icons.js';
-
-
-const typeIcons: Partial<Record<string, TemplateResult>> = {
-  CBR: circuitBreakerIcon,
-  DIS: disconnectorIcon,
-  CTR: currentTransformerIcon,
-  VTR: voltageTransformerIcon,
-  ERS: earthSwitchIcon,
-};
-
-function typeStr(condEq: Element): string {
-  return condEq.getAttribute('type') === 'DIS' &&
-    condEq.querySelector('Terminal')?.getAttribute('cNodeName') === 'grounded'
-    ? 'ERS'
-    : condEq.getAttribute('type') ?? '';
-}
-
-export function typeIcon(condEq: Element): TemplateResult {
-  return typeIcons[typeStr(condEq)] ?? generalConductingEquipmentIcon;
-}
 
 @customElement('action-component')
 export class ActionComponent extends LitElement {
-  @property({ type: Element })
-  element!: Element;
-
-  @property({type: String})
-  icon = '';
+  @property()
+  icon: TemplateResult | string  = '';
 
   @property({ type: String })
   size: 'small' | 'medium' | 'large' = 'medium';
@@ -53,10 +23,50 @@ export class ActionComponent extends LitElement {
   @property({type: String})
   trimText: 'left' | 'right' = 'left';
 
+  @internalProperty()
+  get moveDistance(): string {
+    return this.size === 'large' ? '60px' : '52px';
+  } 
+
+
   render(): TemplateResult {
     return html`<div class="container ${this.size}" tabindex="0">
-      <mwc-icon-button class="icon ${this.size}" icon="${this.icon}">${this.icon === '' ? typeIcon(this.element) : ''}</mwc-icon-button>
-      <slot></slot>
+      <mwc-icon class="icon ${this.size}">${this.icon}</mwc-icon>
+      <slot>
+        <style>
+        .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(1)) {
+          transform: translate(0px, -${this.moveDistance});
+        }
+    
+        .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(2)) {
+          transform: translate(0px, ${this.moveDistance});
+        }
+    
+        .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(3)) {
+          transform: translate(${this.moveDistance}, 0px);
+        }
+    
+        .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(4)) {
+          transform: translate(-${this.moveDistance}, 0px);
+        }
+
+        .container:focus-within > .icon + ::slotted(mwc-fab:nth-child(5)){
+          transform: translate(${this.moveDistance}, -${this.moveDistance});
+        }
+    
+        .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(6)) {
+          transform: translate(-${this.moveDistance}, ${this.moveDistance});
+        }
+    
+        .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(7)) {
+          transform: translate(-${this.moveDistance}, -${this.moveDistance});
+        }
+    
+        .container:focus-within > .icon  + ::slotted(mwc-fab:nth-child(8)) {
+          transform: translate(${this.moveDistance}, ${this.moveDistance});
+        }
+        </style>
+      </slot>
       <h4 class="footer ${this.trimText}">${this.name}</h4>
     </div>`;
   }
@@ -108,14 +118,9 @@ export class ActionComponent extends LitElement {
       --mdc-icon-size: 50px;
     }
 
-    .container:hover > .icon.small {
+    .container:hover > .icon {
       outline: 2px dashed var(--mdc-theme-primary);
       transition: transform 200ms linear, box-shadow 250ms linear;
-    }
-
-    .container:hover > .icon.medium {
-      outline: 2px dashed var(--mdc-theme-primary);
-      transition: transform 250ms linear, box-shadow 350ms linear;
     }
 
     .container:focus-within > .icon {
@@ -172,79 +177,6 @@ export class ActionComponent extends LitElement {
         opacity 250ms linear;
       pointer-events: auto;
       opacity: 1;
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(1)) {
-      transform: translate(0px, -60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(1)),
-    .icon.small + ::slotted(mwc-fab:nth-child(1)) {
-      transform: translate(0px, -52px);
-    }
-
-    .container:focus-within > .icon.large + ::slotted(mwc-fab:nth-child(2)) {
-      transform: translate(0px, 60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(2)),
-    .icon.small + ::slotted(mwc-fab:nth-child(2)) {
-      transform: translate(0px, 52px);
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(3)) {
-      transform: translate(60px, 0px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(3)),
-    .icon.small + ::slotted(mwc-fab:nth-child(3)) {
-      transform: translate(52px, 0px);
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(4)) {
-      transform: translate(-60px, 0px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(4)),
-    .icon.small + ::slotted(mwc-fab:nth-child(4)) {
-      transform: translate(-52px, 0px);
-    }
-
-    .container:focus-within > {
-      transform: translate(60px, -60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(5)),
-    .icon.small + ::slotted(mwc-fab:nth-child(5)) {
-      transform: translate(52px, -52px);
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(6)) {
-      transform: translate(-60px, 60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(6)),
-    .icon.small + ::slotted(mwc-fab:nth-child(6)) {
-      transform: translate(-52px, 52px);
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(7)) {
-      transform: translate(-60px, -60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(7)),
-    .icon.small + ::slotted(mwc-fab:nth-child(7)) {
-      transform: translate(-52px, -52px);
-    }
-
-    .container:focus-within > .icon.large  + ::slotted(mwc-fab:nth-child(8)) {
-      transform: translate(60px, 60px);
-    }
-
-    .container:focus-within > .icon.medium + ::slotted(mwc-fab:nth-child(8)),
-    .icon.small + ::slotted(mwc-fab:nth-child(8))
-     {
-      transform: translate(52px, 52px);
     }
 
   `;
